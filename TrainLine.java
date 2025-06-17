194c2a660f45e9facfb966fabdbf7f3a2867f939
JHarperluc7 <harperj16@yahoo.com>
2025-06-17 17:53:21 +0000

    UpdateOriginalTrainLine

--- TrainLine.java
+++ TrainLine.java
@@ -8,7 +8,9 @@ public class TrainLine {
     private Station head;
     /** Current number of stations in the line */
     private int numberOfStations;
-
+    /** Subtracts -1 to avoid "magic numbers" */
+    private static final int NOT_FOUND = -1;
+    
     /** Basic constructor */
     public TrainLine(String name) {
         this.name = name;
@@ -49,7 +51,7 @@ public class TrainLine {
         }
         this.numberOfStations = this.numberOfStations+1; 
         // or this.numberOfStations++;
-        // or this.numberOfStatiosn += 1;
+        // or this.numberOfStations += 1;
     } // method add
 
     /**
@@ -69,6 +71,47 @@ public class TrainLine {
         }
         return counter;
     } // method countStations
+
+    /**
+ * Checks the train line to see if it contails a station with the name
+ * Returns true if found, false if not found
+ */
+public boolean contains(String name) { // Method to check existance of station
+    boolean found = false; // Use of flag to track if station is found
+    Station cursor = this.head; // Starts from the head station
+
+    while (cursor != null && !found) { // Loops to the end or until station found
+        if (cursor.getName().equals(name)) { // Uses .equals to compare contents of oject
+            found = true; // Station name matches
+        }
+        cursor = cursor.getNext(); // Moves to the next station
+    }
+
+    return found; // Return result T/F
+} // method contains
+
+/**
+ * Returns the index of the station and the station name.
+ * If NOT found returns -1 
+ */
+    public int indexOf(String name) { // Method to return station index
+    int index = 0; // Counter for position/placement
+    Station cursor = this.head; // Start at the head
+
+    while (cursor != null) { // Traverses all stations given
+        if (cursor.getName().equals(name)) { // Use .equals to compare station names
+            break; // Station found - Loop is broken and exixed
+        }
+        cursor = cursor.getNext(); // Move to next station
+        index++; // Adds to index
+    }
+
+    if (cursor == null) { // If reached end without finding the station
+        index = NOT_FOUND; // Set index to -1 (NOT_FOUND)
+    }
+
+    return index; // Return the index or -1 if not found
+} // method indexOf
 
 
     /**
